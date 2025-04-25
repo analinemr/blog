f43d91cc497f26a169f891eb832243ce669be479
analinemr <119958482+analinemr@users.noreply.github.com>
2025-04-25 19:26:35 -0300

    alt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,15 @@
-<!-- resources/views/app.blade.php -->
+<!-- resources/views/child.blade.php -->
 
 @extends('layouts.app')
 
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @@parent
+
+    <p>This is appended to the master sidebar.</p>
+@endsection
+
 @section('content')
-    <p>Meu conteúdo.</p>
+    <p>This is my body content.</p>
 @endsection
